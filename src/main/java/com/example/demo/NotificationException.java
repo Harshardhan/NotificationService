d7d20f5e58c6073549519c09353e35d4461db660
harshardhan <harshavardhan.harish@gmail.com>
2025-03-13 10:57:44 +0530

    new update

--- src/main/java/com/example/demo/NotificationException.java
+++ src/main/java/com/example/demo/NotificationException.java
@@ -3,6 +3,6 @@ package com.example.demo;
 public class NotificationException extends Exception{
 
 	public NotificationException(String message, Exception e) {
-		super(message);
+		super(message,e);
 	}
 }
